336127f013bdd470ab5ec72ee52a96a3b575e5e3
synapsys0 <cclothier@outlook.com>
2018-09-07 08:42:21 -0700

    commit message

---
+++ List Less than Ten.py
@@ -0,0 +1,6 @@
+# this one is different!
+name = input("Give me your name: ")
+age = int(input("Give me your age: "))
+oldage = str((2018 - age)+100)
+
+print(name + ", you will be 100 years old in: " + oldage )
