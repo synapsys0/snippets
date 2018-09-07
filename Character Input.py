83dd19a53c0f4ab5f61b2d06736c45aa50e239c5
synapsys0 <42789689+synapsys0@users.noreply.github.com>
2018-09-07 08:35:27 -0700

    Add files via upload

---
+++ Character Input.py
@@ -0,0 +1,5 @@
+name = input("Give me your name: ")
+age = int(input("Give me your age: "))
+oldage = str((2018 - age)+100)
+
+print(name + ", you will be 100 years old in: " + oldage )
---
+++ Odd or Even.py
@@ -0,0 +1,6 @@
+number = int(input("Give me a number: "))
+
+if number % 2 == 0:
+    print("The number is EVEN")
+else:
+    print("The number is ODD")
